0e362e3bc87cdfd4353760ce09f415942a2c168f
Barbara <z4beth@gmail.com>
2012-02-28 01:11:14 -0500

    Updating CSS

--- apps/feedback/processes.py
+++ apps/feedback/processes.py
@@ -31,6 +31,6 @@ class FeedbackPost( URIHandler ):
             url = os.environ['HTTP_REFERER']
 
         # Email Barbara
-        Email.emailBarbara( '<p>[%s]:</p> <p>%s: %s</p> <p>%s</p> <p>%s</p> <p>%s</p><p>%s</p><p>%s</p>' % (title, owner, email, text, store_name, store_url, url) )
+        Email.emailBarbara( '<p>[%s]:</p> <p>%s: %s</p> <p>%s</p> <p>%s</p> <p>%s</p><p>%s</p>' % (title, owner, email, text, store_name, store_url, url) )
 
         self.response.out.write("Thanks!") 
